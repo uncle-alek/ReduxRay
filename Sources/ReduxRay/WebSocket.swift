4be42b50e581367e08e1cf059343e16676959063
Aleksey Yakimenko <aleksey.yakimenko@agoda.com>
2022-08-15 19:02:15 +0700

    Add IP address searching

--- Sources/ReduxRay/WebSocket.swift
+++ Sources/ReduxRay/WebSocket.swift
@@ -17,7 +17,7 @@ func connectIfNeeded() {
 
 func connect() {
     let urlSession = URLSession(configuration: .default)
-    webSocket = urlSession.webSocketTask(with: URL(string: "ws://127.0.0.1:3002/taga")!)
+    webSocket = urlSession.webSocketTask(with: URL(string: "ws://\(getIPAddress()!):3002/redux")!)
     webSocket.resume()
 }
 
@@ -25,4 +25,28 @@ func send(_ string: String) {
     webSocket.send(.string(string)) { error in
         print("Error: \(String(describing: error)) occurred while connecting via websocket!")
     }
+}
+
+func getIPAddress() -> String? {
+    var address: String?
+    var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+    if getifaddrs(&ifaddr) == 0 {
+        var ptr = ifaddr
+        while ptr != nil {
+            defer { ptr = ptr?.pointee.ifa_next }
+            
+            let interface = ptr?.pointee
+            let addrFamily = interface?.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                let name: String = String(cString: (interface?.ifa_name)!)
+                if name == "en0" {
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface?.ifa_addr, socklen_t((interface?.ifa_addr.pointee.sa_len)!), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+    }
+    return address
 }
